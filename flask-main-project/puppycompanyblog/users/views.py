024d28976aa36f26cd59c9d79896ab1a32b05e29
Pawel Kiszczak <pawel.kiszczak@icloud.com>
2024-04-26 19:28:06 +0200

    finish views

--- flask-main-project/puppycompanyblog/users/views.py
+++ flask-main-project/puppycompanyblog/users/views.py
@@ -1,4 +1,6 @@
 # puppycompanyblog/users/views.py
+import os
+
 from time import sleep
 
 from flask import render_template, url_for, flash, redirect, request, Blueprint
@@ -51,3 +53,41 @@ def login():
             return redirect(next)
         
     return render_template('login.html', form=form)
+
+@users.route('/account', methods=["GET", 'POST'])
+@login_required
+def account():
+    form = UpdateUserForm()
+
+    if form.validate_on_submit():
+        if form.picture.data:
+            username = current_user.username
+            pic = add_profile_pic(form.picture.data, username)
+
+            current_user.profile_image = pic
+
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('User account updated!')
+        return redirect(url_for("users.account"))
+    
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    
+    profile_image = url_for('static', 
+                            filename=os.path.join('profile_pics', current_user.profile_image))
+    
+    return render_template('account.html', 
+                           profile_image=profile_image, 
+                           form=form)
+
+@users.route('/<username>')
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    blog_posts = BlogPost.query.filter_by(author=user)\
+        .order_by(BlogPost.date.desc())\
+        .paginate(page=page, per_page=5)
+    return render_template('user_blog_posts.html', blog_posts=blog_posts, user=user)
