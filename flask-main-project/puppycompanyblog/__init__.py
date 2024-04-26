fccf7f9d697f4272b4e60f678a6de524ba7389f3
Pawel Kiszczak <pawel.kiszczak@icloud.com>
2024-04-26 17:21:24 +0200

    error handling: 403, 404

--- flask-main-project/puppycompanyblog/__init__.py
+++ flask-main-project/puppycompanyblog/__init__.py
@@ -1,6 +1,8 @@
 # puppycompanyblog/__init__.py
 from flask import Flask
 from puppycompanyblog.core.views import core
+from puppycompanyblog.error_pages.handlers import error_pages
 
 app = Flask(__name__)
 app.register_blueprint(core)
+app.register_blueprint(error_pages)
